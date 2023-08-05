2a9e99b31625796de0f219a22cc623336a92fba9
Martin Lucas Noel <martin.noel@live.com>
2023-08-05 19:44:39 +0200

    chore: skeleton process of controls
    - moved the selecedArea_X lower and made the script spawn the gameobjects higher
    - Started on the control, finished by making keys go to specific spots

--- Assets/Scripts/PlaceToken.cs
+++ Assets/Scripts/PlaceToken.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,50 @@ using UnityEngine;
 public class PlaceToken : MonoBehaviour
 {
     public GameObject Token;
-    public GameObject SelectedArea;
+    public GameObject DefaultArea;
+    public GameObject[] SelectedArea;
+    //get&set for pickingArea
+
+    private void Start()
+    {
+        DefaultArea.SetActive(true);
+
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 SelectedArea_1Postion = SelectedArea.transform.position;
+        Controls();
+    }
+
+
+    public void Controls()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            DefaultArea.SetActive(false);
+            DefaultArea = SelectedArea[3];
+            DefaultArea.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            DefaultArea.SetActive(false);
+            DefaultArea = SelectedArea[0];
+            DefaultArea.SetActive(true);
+        }
+
+        Vector3 SelectedArea_1Postion = DefaultArea.transform.position;
+        //Debug.Log(SelectedArea_1Postion);
         //To-DO: Make it so that when you press a button it counts up words. So that you can change where you want to place it with pressing one button.
 
+        Debug.Log(DefaultArea.activeInHierarchy);
         if (Input.GetKeyDown(KeyCode.Space))
-            Instantiate(Token, SelectedArea_1Postion, Quaternion.identity);
+        {
+            DefaultArea.SetActive(false);
+            Debug.Log(DefaultArea.activeInHierarchy);
+            Instantiate(Token, (SelectedArea_1Postion + new Vector3(0f, 3.12f, 0f)), Quaternion.identity);
+        }
     }
 }
