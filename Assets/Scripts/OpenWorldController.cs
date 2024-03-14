0e81452486128cc91a6f83a7cf366b2e7aa36574
Martin Lucas Noel <martin.noel@live.com>
2024-03-14 16:29:56 +0100

    chore: craeted OpenWorld scene and open world player controller script

---
+++ Assets/Scripts/OpenWorldController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class OpenWorldController : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    //public float rotationSpeed = 100f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Movement
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * moveSpeed;
+
+        // Rotation
+        if (movement != Vector3.zero)
+        {
+            //Quaternion newRotation = Quaternion.LookRotation(movement);
+           // rb.rotation = Quaternion.Slerp(rb.rotation, newRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
