c54100753cf6cd01f4d66715816baa0d3dda8d20
Martin Lucas Noel <martin.noel@live.com>
2024-08-11 16:21:16 +0200

    chore: polished movement functionality & animation

--- Assets/Scripts/OpenWorldController.cs
+++ Assets/Scripts/OpenWorldController.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class OpenWorldController : MonoBehaviour
 {
-    public float walkSpeed = 5f;
-    public float sprintSpeed = 15;
-    public float rotationSpeed = 10f;
-    public float moveSpeed;
+    private readonly float walkSpeed = 4f;
+    private readonly float sprintSpeed = 8;
+    private readonly float rotationSpeed = 10f;
+
+    public float MoveSpeed { get; set; }
     private Rigidbody rb;
     private Animator animator;
+    public bool animatorJump;
 
 
     void Start()
@@ -23,23 +25,46 @@ public class OpenWorldController : MonoBehaviour
         float moveVertical = Input.GetAxis("Key - Vertical");
         float moveUp = Input.GetAxis("Key - Jump");
 
-        Vector3 movement = new(moveHorizontal, moveUp, moveVertical);
+        Vector3 movement = new(moveHorizontal, 0f, moveVertical);
 
+        //Animation for jump(glide)
+        if (Input.GetKeyDown(KeyCode.Space) && animatorJump == false)
+        {
+            animator.SetBool("Jump", true);
+            animatorJump = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && animatorJump == true)
+        {
+            animator.SetBool("Jump", false);
+            animatorJump = false;
+        }
+
+        //Functionality and animation of sprint, walk, and idle
         if (Input.GetKey(KeyCode.LeftShift))
-            moveSpeed = sprintSpeed;
+        {
+            MoveSpeed = sprintSpeed;
+            animator.SetBool("Sprint", true);
+            animator.SetBool("Walk", false);
+        }
+        else if (!Input.GetKey(KeyCode.LeftShift) && movement != new Vector3(0f,0f,0f))
+        {
+            MoveSpeed = walkSpeed;
+            animator.SetBool("Sprint", false);
+            animator.SetBool("Walk", true);
+        }
         else
-            moveSpeed = walkSpeed;
-
-        //Debug.Log($"I'm at speed: {moveSpeed}");
+        {
+            animator.SetBool("Sprint", false);
+            animator.SetBool("Walk", false);
+        }
 
         // Set the velocity of the Rigidbody
-        rb.velocity = movement * moveSpeed;
-
+        rb.velocity = movement * MoveSpeed;
         // Set the animator Speed parameter based on current velocity magnitude
         animator.SetFloat("Speed", rb.velocity.magnitude);
 
         // Output debug information to ensure correct speed setting
-        Debug.Log($"Velocity Magnitude: {rb.velocity.magnitude}, Speed Parameter: {animator.GetFloat("Speed")}");
+        //Debug.Log($"Velocity Magnitude: {rb.velocity.magnitude}, Speed Parameter: {animator.GetFloat("Speed")}");
 
         // Rotation
         if (movement != Vector3.zero)
